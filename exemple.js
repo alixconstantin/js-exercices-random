fb814d369f97a5dca933a0959d7f0b778d2651ae
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2021-05-07 02:48:25 +0200

    plop

--- exemple.js
+++ exemple.js
@@ -1,11 +1,11 @@
-
+//Les boucles, mais encore ? //
 let cats =
          [  'black cat',
             'white cat',
             'blue cat'
             ];
 
-//boucle
+
 for (var cat = 0; cat < cats.length; cat++) {
 
     console.log(cats[cat]); 
@@ -38,12 +38,32 @@ for (let pet = 0; pet < cats.length; pet++){
         }, pet * 1000); // 1000 millisecondes 
      }
 /*
-You have a really beautifulblack cat
+You have a really beautiful black cat
 
-You have a really beautifulwhite cat
+You have a really beautiful white cat
 
-You have a really beautifulblue cat
+You have a really beautiful blue cat
 */
+
+////////////////////////////////
+// La méthode : map
+// elle ressemble beaucoup a forEach, sauf que forEach ne retourne rien
+// /!/  // cette methode ne retourne RIEN
+// const machin = cats.forEach(function(meow){return meow});
+// /!/  // cette methode ne retourne RIEN
+
+// map va toujours renvoyer un nouveau tableau, avec autant d'éléments que celui de base
+
+const truc = cats.map(function(meow){
+    //ce que me return a l'interieur de map fera partie du nouveau tableau
+});
+
+const prout = cats.map(function(meow){
+        
+    return " You really have a beautiful " + meow ;
+});
+
+console.log(prout); 
 
 ////////////////////////////////
 
